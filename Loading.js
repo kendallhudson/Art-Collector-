dfb49d612f780c19cc91bff18585c564b7284679
Kendall Hudson <64994077+kendallhudson@users.noreply.github.com>
2022-12-08 18:27:33 -0600

    Uploaded all files EXCEPT node and build folders

---
+++ Loading.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+/**
+ * Create and export a component called Loading which uses this static HTML template:
+ *
+ * <div id="loading">
+ *   <h2 className="message">Searching...</h2>
+ * </div>
+ */
+const Loading = () => {
+  <div id="loading">
+    <h2 className="message">Searching...</h2>
+  </div>;
+};
+
+export { default as Loading } from './Loading';
